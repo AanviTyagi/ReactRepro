7045f1a2622d12ac013177f8ba6d5f530de9ef4d
AanviTyagi <tyagiaanvi8@gmail.com>
2025-06-19 09:26:59 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://shreeraam1234:shreeraam1234@cluster0.klxwmmw.mongodb.net/reprocare?retryWrites=true&w=majority&appName=Cluster0',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://**********************cluster0.klxwmmw.mongodb.net/reprocare?retryWrites=true&w=majority&appName=Cluster0',
   JWT_SECRET: process.env.JWT_SECRET || 'reprocare_jwt_secret_2024',
   PORT: process.env.PORT || 5002
 }; 
